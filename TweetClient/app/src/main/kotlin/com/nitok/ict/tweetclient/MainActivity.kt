b2ee9f4fc4942c7e7a1b07201403dd5eb5c909f4
mitohato <mitohato14@gmail.com>
2018-04-22 16:42:47 +0900

    Add TweetNavigator to MainActivity

--- TweetClient/app/src/main/kotlin/com/nitok/ict/tweetclient/MainActivity.kt
+++ TweetClient/app/src/main/kotlin/com/nitok/ict/tweetclient/MainActivity.kt
@@ -3,7 +3,14 @@ package com.nitok.ict.tweetclient
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TweetNavigator {
+    override fun onStartWriteTweet() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+    
+    override fun onPostTweet() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
