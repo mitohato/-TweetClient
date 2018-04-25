5ef0382ec657fa8ad838e61b2e2d00cac44a2d83
mitohato <mitohato14@gmail.com>
2018-04-26 08:38:46 +0900

    Add new TweetViewModel inherit BaseTweetViewModel

--- TweetClient/app/src/main/kotlin/com/nitok/ict/tweetclient/TweetViewModel.kt
+++ TweetClient/app/src/main/kotlin/com/nitok/ict/tweetclient/TweetViewModel.kt
@@ -1,26 +1,9 @@
 package com.nitok.ict.tweetclient
 
 import android.content.Context
-import android.databinding.BaseObservable
-import android.databinding.Bindable
-import android.databinding.ObservableField
+
+class TweetViewModel(context: Context) : BaseTweetViewModel(context) {
 
 
-class TweetViewModel(context: Context) : BaseObservable() {
-    
-    var applicationContext: Context = context.applicationContext
-    
-    val tweetText = ObservableField<String>()
-    
-    var tweetable: Boolean = false
-        @Bindable
-        get() {
-            return true
-        }
-    
-    fun tweet() {
-    }
-    
-    fun start() {
-    }
+
 }
