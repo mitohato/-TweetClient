33a4f5a2f6849fd5e8cbec9ff08ed95a7417e76a
mitohato <mitohato14@gmail.com>
2018-04-26 16:29:19 +0900

    Add var and function

--- TweetClient/app/src/main/kotlin/com/nitok/ict/tweetclient/TweetViewModel.kt
+++ TweetClient/app/src/main/kotlin/com/nitok/ict/tweetclient/TweetViewModel.kt
@@ -3,7 +3,11 @@ package com.nitok.ict.tweetclient
 import android.content.Context
 
 class TweetViewModel(context: Context) : BaseTweetViewModel(context) {
-
-
-
+    private var tweetNavigator: TweetNavigator? = null
+    
+    
+    fun onActivityDestroyed() {
+        tweetNavigator = null
+    }
+    
 }
