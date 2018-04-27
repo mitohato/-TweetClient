f42ab743a87fb55a439b372005456123c9211ab0
mitohato <mitohato14@gmail.com>
2018-04-27 23:51:34 +0900

    Add DataBinding

--- TweetClient/app/src/main/kotlin/com/nitok/ict/tweetclient/TweetFragment.kt
+++ TweetClient/app/src/main/kotlin/com/nitok/ict/tweetclient/TweetFragment.kt
@@ -1,17 +1,28 @@
 package com.nitok.ict.tweetclient
 
 import android.app.Fragment
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.nitok.ict.tweetclient.databinding.FragmentTweetBinding
 
 class TweetFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val view = inflater.inflate(R.layout.activity_main, container, false)
-
-//        val activityMainBinding = DataBindingUtil.bind<ActivityMainBinding>(view)
-//        activityMainBinding?.view_model = TweetViewModel
+    private lateinit var viewModel: TweetViewModel
+    
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(
+                R.layout.activity_main,
+                container, false
+        )
+        
+        val activityMainBinding = DataBindingUtil.bind<FragmentTweetBinding>(view)
+        activityMainBinding?.viewmodel = viewModel
         
         return view
     }
