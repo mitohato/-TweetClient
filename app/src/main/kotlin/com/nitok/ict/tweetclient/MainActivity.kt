f1c973b085b82841dd31ddf36ac36235eeaa53b1
mitohato <mitohato14@gmail.com>
2018-05-15 09:51:50 +0900

    Change fragment type from support fragment

--- app/src/main/kotlin/com/nitok/ict/tweetclient/MainActivity.kt
+++ app/src/main/kotlin/com/nitok/ict/tweetclient/MainActivity.kt
@@ -12,8 +12,8 @@ class MainActivity : AppCompatActivity(), TweetNavigator {
         setContentView(R.layout.activity_main)
 
         if (!TwitterUtils.hasAccessToken(this)) {
-            val transaction = supportFragmentManager.beginTransaction()
             val twitterOAuthFragment = TwitterOAuthFragment.newInstance()
+            val transaction = fragmentManager.beginTransaction()
             transaction.add(R.id.content_frame, twitterOAuthFragment)
             transaction.commit()
         } else {
