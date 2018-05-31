e20c6c14fdf7ee394f21f898fc760addb3ccd8f8
mitohato <mitohato14@gmail.com>
2018-05-31 20:31:06 +0900

    Change return value type

--- app/src/main/kotlin/com/nitok/ict/tweetclient/navigator/TweetNavigator.kt
+++ app/src/main/kotlin/com/nitok/ict/tweetclient/navigator/TweetNavigator.kt
@@ -1,8 +1,10 @@
 package com.nitok.ict.tweetclient.navigator
 
+import kotlinx.coroutines.experimental.Deferred
+
 interface TweetNavigator {
 
     fun onStartWriteTweet()
 
-    fun onPostTweet(tweetText: String)
+    fun onPostTweet(tweetText: String): Deferred<Unit>
 }
